bb89cb638330970076c5de09f8e026957b350dc3
Rençber AKMAN <184839345+RENCBERAKMAN@users.noreply.github.com>
2026-01-04 16:32:21 +0300

    Adding an interface to the project

---
+++ SentinelVault.Application/Interfaces/IAccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using SentinelVault.Domain.Entities;
+using System.Security.Principal;
+
+namespace SentinelVault.Application.Interfaces;
+
+public interface IAccountRepository
+{
+    // Bir hesabı Id'sine göre getir
+    Task<Account?> GetByIdAsync(Guid id);
+
+    // Yeni bir hesap oluştur
+    Task AddAsync(Account account);
+
+    // Hesap bilgilerini güncelle (Örn: Bakiye değişince)
+    Task UpdateAsync(Account account);
+}
--- SentinelVault.Domain/Entities/Account.cs
+++ SentinelVault.Domain/Entities/Account.cs
@@ -1,29 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SentinelVault.Domain.Entities;
 
-namespace SentinelVault.Domain.Entities
+public class Account // Burayı 'public' yaptık
 {
-    internal class Account
-    {
-        // Hesabın kimlik numarası (Eşsiz)
-        public Guid Id { get; set; }
+    // Hesabın kimlik numarası (Eşsiz)
+    public Guid Id { get; set; }
 
-        // Hesap sahibinin adı
-        public string OwnerName { get; set; } = string.Empty;
+    // Hesap sahibinin adı
+    public string OwnerName { get; set; } = string.Empty;
 
-        // Hesap numarası (Örn: TR1234...)
-        public string AccountNumber { get; set; } = string.Empty;
+    // Hesap numarası (Örn: TR1234...)
+    public string AccountNumber { get; set; } = string.Empty;
 
-        // Mevcut bakiye
-        public decimal Balance { get; set; }
+    // Mevcut bakiye
+    public decimal Balance { get; set; }
 
-        // Hesabın oluşturulma tarihi
-        public DateTime CreatedDate { get; set; }
+    // Hesabın oluşturulma tarihi
+    public DateTime CreatedDate { get; set; }
 
-        // Hesap aktif mi? (Siber güvenlik için veriyi silmeyiz, pasife çekeriz)
-        public bool IsActive { get; set; } = true;
-    }
+    // Hesap aktif mi?
+    public bool IsActive { get; set; } = true;
 }
