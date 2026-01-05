3ea48eb75d90cbf1b8b0b62d48b51129badfde62
Rençber AKMAN <184839345+RENCBERAKMAN@users.noreply.github.com>
2026-01-05 14:07:26 +0300

    feat: implement DbContext and repository interfaces for financial core

---
+++ SentinelVault.Persistence/Contexts/SentinelVaultDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SentinelVault.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SentinelVault.Persistence.Contexts;
+
+// 1. Başına 'public' ekledik.
+// 2. ': DbContext' ekleyerek EF Core yeteneklerini aldık.
+public class SentinelVaultDbContext : DbContext
+{
+    public SentinelVaultDbContext(DbContextOptions<SentinelVaultDbContext> options) : base(options)
+    {
+    }
+
+    // Veritabanındaki 'Accounts' tablomuz
+    public DbSet<Account> Accounts { get; set; }
+}
