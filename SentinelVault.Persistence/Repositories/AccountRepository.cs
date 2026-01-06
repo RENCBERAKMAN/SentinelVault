153077480729d48c56e65c7794ff028c5b3484ed
Rençber AKMAN <184839345+RENCBERAKMAN@users.noreply.github.com>
2026-01-06 16:52:26 +0300

    Add

---
+++ SentinelVault.Persistence/Repositories/AccountRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SentinelVault.Application.Interfaces;
+using SentinelVault.Domain.Entities;
+using SentinelVault.Persistence.Contexts;
+
+namespace SentinelVault.Persistence.Repositories;
+
+public class AccountRepository : IAccountRepository
+{
+    private readonly SentinelVaultDbContext _context;
+
+    public AccountRepository(SentinelVaultDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Account?> GetByIdAsync(Guid id)
+    {
+        return await _context.Accounts.FindAsync(id);
+    }
+
+    public async Task AddAsync(Account account)
+    {
+        await _context.Accounts.AddAsync(account);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Account account)
+    {
+        _context.Accounts.Update(account);
+        await _context.SaveChangesAsync();
+    }
+}
