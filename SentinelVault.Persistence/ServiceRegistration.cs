8353ccb767c5a25d141f4e9d50856284d5bae718
Rençber AKMAN <184839345+RENCBERAKMAN@users.noreply.github.com>
2026-01-07 15:00:14 +0300

    implement AccountRepository and Persistence service registration

---
+++ SentinelVault.Persistence/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SentinelVault.Application.Interfaces;
+using SentinelVault.Persistence.Repositories;
+
+namespace SentinelVault.Persistence;
+
+public static class ServiceRegistration
+{
+    public static void AddPersistenceServices(this IServiceCollection services)
+    {
+        // Sisteme diyoruz ki: "Birisi IAccountRepository arayüzünü çağırırsa, 
+        // ona gerçek işi yapan AccountRepository sınıfını ver."
+        services.AddScoped<IAccountRepository, AccountRepository>();
+    }
+}
